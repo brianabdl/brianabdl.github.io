f8ff46f356e4c332451afe3aba9c417f8b804fb3
brianabdl <mohammadbrianabdillah@gmail.com>
2024-01-07 12:15:13 +0700

    setup: github actions

--- src/app/body.js
+++ src/app/body.js
@@ -4,7 +4,7 @@ const Body = () => {
     return (
         <main className='h-screen overflow-auto  flex flex-col items-center justify-center -mt-20'>
             <div className='flex flex-col items-center'>
-                <h1 className='text-4xl lg:text-6xl font-bold text-center'>Hi, I'm Brian</h1>
+                <h1 className='text-4xl lg:text-6xl font-bold text-center'>Hi, I&apos;m Brian</h1>
                 <p className='text-xl lg:text-3xl lg:pt-4 text-center'>Student at SMK Pawyatan Daha 1 Kediri.</p>
             </div>
             <div className='flex flex-row justify-center items-center mt-8'>
