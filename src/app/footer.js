9c49ed4e1d586a7c8e7aa4a586b501ceb6c1d370
Muhammad Brian Abdillah <mohammadbrianabdillah@gmail.com>
2024-01-10 04:32:34 +0000

    Remove unnecessary class from footer element

--- src/app/footer.js
+++ src/app/footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className='w-full mx-auto px-4 py-8 bg-gray-700'>
+        <footer className='w-full px-4 py-8 bg-gray-700'>
             <p className='text-center text-gray-400 text-sm'>© 2023 Brian. All rights reserved.</p>
         </footer>
     );
