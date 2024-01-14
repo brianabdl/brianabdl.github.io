1dd58ffd472a121ea57277cd864df8382c0eb752
brianabdl <mohammadbrianabdillah@gmail.com>
2024-01-14 09:01:29 +0700

    Remove unnecessary 'icon' call

--- src/app/about/sosmed.js
+++ src/app/about/sosmed.js
@@ -1,5 +1,4 @@
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {icon} from "@fortawesome/fontawesome-svg-core";
 import {faEnvelope} from "@fortawesome/free-solid-svg-icons";
 import {faGithub, faInstagram, faLinkedin, faTwitter} from "@fortawesome/free-brands-svg-icons";
 
@@ -36,7 +35,7 @@ export const SocialMedia = (props) => {
         <div className="flex flex-col justify-center items-center space-y-2">
             <h2 className='space-y-2 text-lg font-bold'>{props.name}</h2>
             <a href={props.link} target="_blank" rel="noreferrer">
-                <FontAwesomeIcon icon={icon(props.icon)} alt={props.name} width={50} height={50}/>
+                <FontAwesomeIcon icon={props.icon} alt={props.name} width={50} height={50}/>
             </a>
         </div>
     )
