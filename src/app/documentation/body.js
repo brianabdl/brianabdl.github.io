efac87e07ba004bd752201bd32106ca8d450a21e
brianabdl <mohammadbrianabdillah@gmail.com>
2024-01-14 09:00:27 +0700

    Refine rank status

--- src/app/documentation/body.js
+++ src/app/documentation/body.js
@@ -7,17 +7,15 @@ const Body = () => {
                 <h1 className="text-4xl font-bold pt-4">Dokumentasi</h1>
                 <div className="grid grid-cols-2 md:grid-cols-3 gap-4 m-4">
                     {documentation.map((el, i) => (
-                        <>
-                            <div className="flex flex-col items-center justify-start space-y-2" key={i}>
-                                <img src={el.image} alt={el.name}
-                                     className="rounded-lg shadow-lg object-cover"/>
-                                <div className="flex flex-col items-center justify-center">
-                                    <h2 className="text-lg text-balance font-bold">{el.name}</h2>
-                                    <p className="text-md">{el.date}</p>
-                                    <p className="text-md">{el.place}</p>
-                                </div>
+                        <div className="flex flex-col items-center justify-start space-y-2" key={i}>
+                            <img src={el.image} alt={el.name}
+                                 className="rounded-lg shadow-lg object-cover"/>
+                            <div className="flex flex-col items-center justify-center">
+                                <h2 className="text-lg text-balance font-bold">{el.name}</h2>
+                                <p className="text-md">{el.date}</p>
+                                <p className="text-md">{el.place}</p>
                             </div>
-                        </>
+                        </div>
                     ))}
                 </div>
             </main>
