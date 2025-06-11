864b266990cf0a5c6ccd187071a6aa5e631fddb8
brianabdl <mohammadbrianabdillah@gmail.com>
2025-06-11 20:11:57 +0700

    fix routing issue

--- src/app/wicarasatwa/app/page.js
+++ src/app/wicarasatwa/app/page.js
@@ -1,16 +1,17 @@
 "use client";
 import { useEffect } from 'react';
 
+const APK_URL = 'https://github.com/brianabdl/WicaraSatwa/releases/latest/download/wicarasatwa.apk';
 export default function AppRedirect() {
   useEffect(() => {
-    window.location.href = '/api/download-apk';
+    window.location.href = APK_URL;
   }, []);
   return (
     <div>
       <p>Redirecting to download...</p>
       <p>
         If the download does not start automatically,&nbsp;
-        <a href="/api/download-apk">click here to download the APK manually</a>.
+        <a href={APK_URL}>click here to download the APK manually</a>.
       </p>
     </div>
   );
